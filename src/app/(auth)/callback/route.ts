13dda3bb293c5256449c9dd182f419b871d16025
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2026-07-22 20:12:11 +0530

    :(

--- src/app/(auth)/callback/route.ts
+++ src/app/(auth)/callback/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import { createClientServer } from '@/lib/supabase'
 import prisma from '@/lib/prisma'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
